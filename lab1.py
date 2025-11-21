0d37398197cbc5905d632b535855109d812db38b
jpp8 <jtejada@broncos.uncfsu.edu>
2025-11-21 16:26:32 -0500

    done

--- lab1.py
+++ lab1.py
@@ -6,28 +6,32 @@ The following n lines are the data lines, each line has a number.
 The program reads the file "lab1_file.txt", and calculate average number of all those data line.
 """
 
+
 def computer_average(filename):
-    # create a file object called "file_obj" in read mode
+    # create a file object called "file_obj"
+    # the file object opens the file passed by the argument "filename"
+    # use open mode "r"
     file_obj = open(filename, "r")
 
-    # read first line, which is number of data lines
+    # read file line, which is number of lines
     first_line = file_obj.readline()
+    # convert the variable `first_line` into an integer called `nums_line`
     nums_line = int(first_line.strip())
 
     # create a list to wrap all following numbers
     numbers = []
 
     for i in range(nums_line):
-        # read one line in each iteration, convert to float
-        line = file_obj.readline()
-        number = float(line.strip())
-        numbers.append(number)
+        # read one line in each iteration, read as a float number
+        num = float(file_obj.readline().strip())
+        # append the float number into the list `numbers`
+        numbers.append(num)
 
     # close the file object
     file_obj.close()
 
-    # calculate the average
-    avg = sum(numbers) / len(numbers) if numbers else 0.0
+    # calculate the average of all numbers in the list, save the result to variable `avg`
+    avg = sum(numbers) / len(numbers)  # will raise ZeroDivisionError if nums_line == 0
     return avg
 
 
